08322eec07f51a3a56705a2601413f6da7e3339d
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-08-26 10:46:32 -0300

    feat: create subtrair test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar } from "../src/calculadora.js";
+import { somar, subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -13,4 +13,18 @@ describe("Calculadora", () => {
         // Assert (Verificar)
         expect(resultado). toBe(5);
     });
+
+        test("Deve subtrair dois números corretamente", () => {
+        // AAA
+        // Arranjar (Preparar)
+        const a = 13;
+        const b = 3;
+
+        // Act (Executar)
+        const resultado = subtrair(a, b);
+        
+        // Assert (Verificar)
+        expect(resultado). toBe(10);
+    });
 });
+
